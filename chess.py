169ece3430dbbd68f49eab860b73df32b2505509
lamkhanhtran <tranphamlamkhanh@gmail.com>
2020-07-19 15:43:51 +0700

    hello

--- chess.py
+++ chess.py
@@ -30,9 +30,96 @@ def setup_Board():
 
 def board_Display():
     pass
+    # for i in range(8):
+	#     for j in range(8):
+	# 	    board_State[i][j].pack();
+# tic_tac_toe[0].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[0][0])))
+# tic_tac_toe[0].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[0][1])))
+# tic_tac_toe[0].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[0][2])))
+# tic_tac_toe[1].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[1][0])))
+# tic_tac_toe[1].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[1][1])))
+# tic_tac_toe[1].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[1][2])))
+# tic_tac_toe[2].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[2][0])))
+# tic_tac_toe[2].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[2][1])))
+# tic_tac_toe[2].append(Button(root, width=10, height=5, command=lambda : disableBtn(tic_tac_toe[2][2])))
+
+# class Game:
+
+#     def __init__(self):
+
+#         self.game_Board = {}
+#         self.place_Pieces()
+#         self.Moving()
+
+#     def setup_Board():
+
+#         for i in range(0, 8):
+#             board_State.append([])
+#             for j in range(0, 8):
+#                 board_State[i].append(Button(Game, width=10, height=5, command=lambda : nothing()))
+
+#     def place_Pieces(self):
+
+#         for i in range(0, 8):
+#                 self.game_Board[()]
+
+
+#     def board_Display(self):
 
 setup_Board()
 board_Display()
 
+imgx = 20
+imgy = 20
+pickup = False
+chess_Pieces_img[0] = PhotoImage(file="c:/Users/admin/Desktop/python-prj/pawn.png")
+chess_pieces = game_Board.create_image(imgx, imgy, anchor=NW, image=chess_Pieces_img[0])
+
+def coord_pickup(e):
+
+    global chess_Pieces_img, player_Turn, imgx, imgy, pickup
+    if e.x>imgx and e.x<imgx+70 and e.y>imgy and e.y<imgy+70:
+        chess_Pieces_img[0] = PhotoImage(file="c:/Users/admin/Desktop/python-prj/pawn.png")
+        chess_pieces = game_Board.create_image(e.x, e.y, image=chess_Pieces_img[0])
+        pickup = True
+
+def move(e):
+    
+    global chess_Pieces_img, player_Turn, pickup
+    if pickup:
+        chess_Pieces_img[0] = PhotoImage(file="c:/Users/admin/Desktop/python-prj/pawn.png")
+        chess_pieces = game_Board.create_image(e.x, e.y, image=chess_Pieces_img[0])
+
+def coord_drop(e):
+
+    global chess_Pieces_img, player_Turn, imgx, imgy, pickup
+    if pickup:
+        chess_Pieces_img[0] = PhotoImage(file="c:/Users/admin/Desktop/python-prj/pawn.png")
+        new_imgx = int((e.x-20)/70)*70+20
+        new_imgy = int((e.y-20)/70)*70+20
+        if new_imgx>=20 and new_imgx<580 and new_imgy>=20 and new_imgy<580:
+            imgx = new_imgx
+            imgy = new_imgy
+        chess_pieces = game_Board.create_image(imgx, imgy, anchor=NW, image=chess_Pieces_img[0])
+        pickup = False
+
+
+game_Board.bind("<Button-1>", coord_pickup)
+game_Board.bind("<B1-Motion>", move)
+game_Board.bind("<ButtonRelease-1>", coord_drop)
+
+# class PAWN:
+
+#     def __init__(self, x, y, number, color):
+
+#         self.x = x
+#         self.y = y
+#         self.number = number
+#         self.color = color
+#         self.first_move = false
+
+#     def showAvailable(self):
+
+#         if(
 
 root.mainloop()
